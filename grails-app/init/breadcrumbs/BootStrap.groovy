bf32da4db2d39eb8ba13ad49790ef41bc77ac447
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-08-22 18:50:27 +0300

    Added JSON with data for DB. Easy cleanup.

--- grails-app/init/breadcrumbs/BootStrap.groovy
+++ grails-app/init/breadcrumbs/BootStrap.groovy
@@ -4,10 +4,15 @@ import groovy.json.JsonSlurper
 
 class BootStrap {
     def init = { servletContext ->
-        def data = new JsonSlurper().parse(new File(ProjectUtils.getCorrectPathOS("grails-app\\assets\\resources\\Articles.json")))
-        data.each{
+        def articles = new JsonSlurper().parse(new File(ProjectUtils.getCorrectPathOS("src\\main\\resources\\Articles.json")))
+        articles.each{
             Article article = new Article(it)
             article.save(failOnError:true, flush: true)
+        }
+        def users = new JsonSlurper().parse(new File(ProjectUtils.getCorrectPathOS("src\\main\\resources\\Users.json")))
+        users.each{
+            User user = new User(it)
+            user.save(failOnError:true, flush: true)
         }
     }
     def destroy = {
