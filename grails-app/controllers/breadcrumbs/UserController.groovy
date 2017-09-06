fe39c4cc03b775b61e09ded09d95d1da9d5dddd0
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-09-06 16:48:17 +0300

    Try selectRestrictions with closure delegate

--- grails-app/controllers/breadcrumbs/UserController.groovy
+++ grails-app/controllers/breadcrumbs/UserController.groovy
@@ -4,8 +4,9 @@ import grails.gorm.transactions.Transactional
 import grails.rest.*
 import grails.converters.*
 import grails.rest.RestfulController
+import org.hibernate.Criteria
 
-class UserController extends RestfulController {
+class UserController extends BaseController<User> {
 	static responseFormats = ['json']
     def userService
 
@@ -21,17 +22,50 @@ class UserController extends RestfulController {
         super(resource, readOnly)
     }
 
-    @Override
-    protected List<User> listAllResources(Map params) {
-        listRelatedResources(params)
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        def relatedResourceId = params.keySet().find{it.matches('.*Id$')}
+
+        if(!relatedResourceId){
+            respond(listAllResources(params), model: [("${resourceName}Count".toString()): countResources()])
+        } else {
+            String keyName = "${resourceName}Count".toString()
+            switch (params) {
+                case {params.locationId}: respond(listUsersByLocation(params),
+                                        model: [(keyName): countUserByLocation()]); break
+                case {params.commentId}: respond([findAuthor()],
+                                        model: [(keyName): 1]); break
+                case {params.articleId}: respond([findAuthor()],
+                                        model: [(keyName): 1]); break
+                default: throw new Exception("There is no case with ${relatedResourceId} in ${this.class.name}. " +
+                        "It should be added into switch.")
+            }
+        }
     }
 
-    private List<User> listRelatedResources(Map params){
-        switch (params) {
-            case {params.locationId}: Location.get(params.locationId as Long).users as List; break
-            case {params.commentId}: [Comment.get(params.commentId as Long).auth]; break
-            case {params.articleId}: [Article.get(params.articleId as Long).author]; break
-            default: User.list(params)
+    protected Integer countUserByLocation() {
+        def criteria = User.createCriteria()
+        criteria.get{
+            projections {
+                rowCount()
+            }
+            location{
+                eq("id", params.locationId as Long)
+            }
+        }
+    }
+
+    private List<User> listUsersByLocation(Map params){
+        def criteria = User.createCriteria()
+        criteria.list{
+            selectRestrictions(delegate)
+        }
+    }
+
+    private User findAuthor(){
+        def criteria = User.createCriteria()
+        criteria.get{
+            selectRestrictions(delegate)
         }
     }
 
@@ -42,18 +76,21 @@ class UserController extends RestfulController {
         super.delete()
     }
 
-    @Override
-    protected Integer countResources() {
-        if(params.keySet().find{it.matches('.*Id$')}) {
-            switch (params) {
-                case { params.locationId }: return User.countByLocation(Location.get(params.locationId as Long)); break
-                case { params.commentId }: return 1; break
-                case { params.articleId }: return 1; break
-                default: User.count()
+    Closure selectRestrictions = { delegate ->
+        if (params.locationId) {
+            delegate.location {
+                eq("id", params.locationId as Long)
             }
-        } else {
-            User.count()
+        } else if (params.commentId) {
+            delegate.comments {
+                eq("id", params.commentId as Long)
+            }
+        } else if (params.articleId) {
+            delegate.articles {
+                eq("id", params.articleId as Long)
+            }
         }
     }
 
 }
+
