af2b8e8046ec321ff58d9fcf11fc5bb33e713b02
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-09-07 11:05:05 +0300

    Added base controller and service

--- grails-app/controllers/breadcrumbs/BaseController.groovy
+++ grails-app/controllers/breadcrumbs/BaseController.groovy
@@ -11,12 +11,11 @@ import java.text.SimpleDateFormat
 
 abstract class BaseController<T> extends RestfulController {
 	static responseFormats = ['json']
-    def domainClass
-//    def service = new BaseService()
+    def domainClass = T
 
-//    BaseController() {
-//        super(domainClass)
-//    }
+    BaseController() {
+        super(T)
+    }
 
     BaseController(Class resource) {
         super(resource)
@@ -26,42 +25,8 @@ abstract class BaseController<T> extends RestfulController {
         super(resource, readOnly)
     }
 
-    /**
-     * Lists all resources up to the given maximum
-     *
-     * @param max The maximum
-     * @return A list of resources
-     */
-//    def index(Integer max) {
-//        params.max = Math.min(max ?: 10, 100)
-//        def relatedResourceId = params.keySet().find{it.matches('.*Id$')}
-//        !relatedResourceId ?
-//                respond(listAllResources(params), model: [("${resourceName}Count".toString()): User.count()]):
-//                respond(listRelatedResources(params), model: [("${resourceName}Count".toString()): countResources()])
-//    }
-//
-//    /**
-//     * List all of resource based on parameters
-//     *
-//     * @return List of resources or empty if it doesn't exist
-//     */
-//    @Override
-//    protected List<T> listAllResources(Map params) {
-//        def relatedResourceId = params.keySet().find{it.matches('.*Id$')}
-//        relatedResourceId ? listRelatedResources(params) : resource.list(params)
-//    }
+    void someMethod() {
+        // to do nothing
+    }
 
-    /**
-     * List related resource...
-     *
-     * @return List of resources or empty if it doesn't exist
-     */
-//    protected List<T> listRelatedResources(Map params) {
-//        def criteria = resource.createCriteria()
-//        def resultList = criteria.list{
-//            service.selectRestrictions(delegate)
-//        }
-////        println "!!!!!!!!!!!11 resultList: $resultList"
-//        resultList
-//    }
 }
--- grails-app/services/breadcrumbs/BaseService.groovy
+++ grails-app/services/breadcrumbs/BaseService.groovy
@@ -1,5 +1,6 @@
 package breadcrumbs
 
+import grails.compiler.GrailsCompileStatic
 import grails.gorm.transactions.Transactional
 import org.hibernate.criterion.CriteriaSpecification
 
@@ -7,23 +8,11 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 
 @Transactional
+@GrailsCompileStatic
 class BaseService {
 
-
-//    Closure selectRestrictions = { delegate, params ->
-//        if (params.locationId) {
-//            delegate.location {
-//                eq("id", params.locationId as Long)
-//            }
-//        } else if (params.commentId) {
-//            delegate.comments {
-//                eq("id", params.commentId as Long)
-//            }
-//        } else if (params.articleId) {
-//            delegate.articles {
-//                eq("id", params.articleId as Long)
-//            }
-//        }
-//    }
+    void someMethod() {
+        // to do nothing
+    }
 
 }
