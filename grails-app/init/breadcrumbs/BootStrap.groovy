9d631e98471f618367a16f054a74c5340623ea6b
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-10-08 13:10:48 +0300

    Move Articles.json

--- grails-app/init/breadcrumbs/BootStrap.groovy
+++ grails-app/init/breadcrumbs/BootStrap.groovy
@@ -4,7 +4,8 @@ import groovy.json.JsonSlurper
 
 class BootStrap {
     def init = { servletContext ->
-        def data = new JsonSlurper().parse(new File(ProjectUtils.getCorrectPathOS("grails-app\\assets\\resources\\Articles.json")))
+        def data = new JsonSlurper()
+                .parse(new File(ProjectUtils.getCorrectPathOS("src\\main\\resources\\Articles.json")))
         data.each{
             Article article = new Article(it)
             article.save(failOnError:true, flush: true)
